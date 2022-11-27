259c333c18bae64f8936cda2722644898d3502d0
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-27 12:20:20 +1100

    Hide timer, show on start

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const startQuizBtn = document.getElementById('start-quiz');
 const time = document.getElementById('time');
-
+const timeContainer = document.getElementById('time-container');
 
 let timer;
 let currentTime = 75;
 const startTimer = () => {
+    timeContainer.style.visibility = "visible";
     timer = setInterval(()=>{
         currentTime --;
         time.innerText = currentTime;
     }, 1000)
 }
 
-startQuizBtn.onclick = start;
+startQuizBtn.onclick = startTimer;
