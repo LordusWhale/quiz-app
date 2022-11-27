ae61b76c5ce85f6ca973af8edd80d8abeb680a71
Sam Sweeney <samsweeneyy@gmail.com>
2022-11-27 13:49:12 +1100

    Added clear scores button

--- assets/js/localStorage.js
+++ assets/js/localStorage.js
@@ -1,18 +1,21 @@
-
-
 export const addObjectToArrayLS = (lsKey, item) => {
-    const ls = getObjectFromLS(lsKey);
-    if (!ls) {
-      localStorage.setItem(lsKey, JSON.stringify([item]));
-      return;
-    }
-    const newArray = [...ls, item];
-    localStorage.setItem(lsKey, JSON.stringify(newArray));
-  };
-  
+  const ls = getObjectFromLS(lsKey);
+  if (!ls) {
+    localStorage.setItem(lsKey, JSON.stringify([item]));
+    return;
+  }
+  const newArray = [...ls, item];
+  localStorage.setItem(lsKey, JSON.stringify(newArray));
+};
+
+
+export const deleteAllLS = (lsKey) => {
+  localStorage.setItem(lsKey, null);
+};
+
 
 export const getObjectFromLS = (lsKey) => {
-    const ls = JSON.parse(localStorage.getItem(lsKey));
-    if (ls) return ls;
-    return null;
-  };
+  const ls = JSON.parse(localStorage.getItem(lsKey));
+  if (ls) return ls;
+  return null;
+};
--- assets/js/showScores.js
+++ assets/js/showScores.js
@@ -1,15 +1,24 @@
-import { getObjectFromLS } from "./localStorage.js";
+import { getObjectFromLS, deleteAllLS } from "./localStorage.js";
 import { scoresListPage } from "./pages.js";
 
 const scoresPage = document.getElementById('scores-page');
 const showScoresBtn = document.getElementById('show-scores-btn');
 const scoresList = document.getElementById('scores-list');
 const goBackBtn = document.getElementById('back-btn');
+const clearScoresBtn = document.getElementById('clear-btn');
+
+
+
+
 showScoresBtn.onclick = () => {
     scoresPage.style.visibility = "visible";
 }
 goBackBtn.onclick = () => {
     scoresPage.style.visibility = "hidden";
+}
+clearScoresBtn.onclick = () => {
+    deleteAllLS('score');
+    scoresList.innerHTML = "";
 }
 
 
